8f8d55fbb91230a27ba46c959cd11bf30dadef4f
mrycky23 <rycky_jvm@outlook.com>
2024-03-22 20:10:38 -0500

    autenticacion

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'hola',//process.env.JWT_SECRET
+      secretOrKey: process.env.JWT_SECRET,//process.env.JWT_SECRET
     });
   }
   async validate(payload: any){
